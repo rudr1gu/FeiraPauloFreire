cfafa806bd075e2d1f492097b937dca874019208
Rudr1gu <rodrigo.santos.ii@hotmail.com>
2024-04-25 22:30:53 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const nome = document.getElementById("iname").value;
+const contato = document.getElementById("icontato").value;
+const bio = document.getElementById("ibio").value;
+const img = document.getElementById("iimg").value;
+const fb = document.getElementById("ifacebook").value;
+const insta = document.getElementById("iinstagram").value;
+const spoti = document.getElementById("ispoti").value;
+const spoti = document.getElementById("ilinkedin").value;
+
+
+if(nome.length === 0){
+    alert("Preencha o campo nome")
+}
+
+if(contato.length === 0){
+    alert("Preencha o campo contato")
+}
+
+if(bio.length === 0){
+    alert("Preencha o campo nome.")
+}
+
+
