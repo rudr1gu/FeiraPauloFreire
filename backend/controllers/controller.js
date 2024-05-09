28595492eba2cd731d05c8670b4bd00c99fa20a9
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-05-09 16:06:24 -0300

    ajustes

--- backend/controllers/controller.js
+++ backend/controllers/controller.js
@@ -3,10 +3,17 @@ let visibilidadeBtn = () =>{
 
     btnCamp.forEach(button => {   
         if (button.value === '') {
-            button.style.display = 'none'
+            button.style.display = 'none';
         } 
         
     });
+
+    const links = document.querySelectorAll('.icones');
+
+    links.forEach(item =>{
+        item.dataset.value == "n" ? item.style.display = "none" : console.log(item.value);
+        
+    })
 }
 
 document.addEventListener('DOMContentLoaded', visibilidadeBtn);
